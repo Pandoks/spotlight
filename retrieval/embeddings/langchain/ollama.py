5177291725adb50cf29fb615c7b12fad70f278c3
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-05-19 19:56:33 -0400

    Cleanup embeddings

--- retrieval/embeddings/langchain/ollama.py
+++ retrieval/embeddings/langchain/ollama.py
@@ -1,6 +1,5 @@
 from typing import List, TypedDict
 from langchain_community.embeddings.ollama import OllamaEmbeddings
-from langchain_openai import OpenAIEmbeddings
 
 
 class OllamaEmbedDocumentsConfig(TypedDict):
@@ -10,6 +9,5 @@ class OllamaEmbedDocumentsConfig(TypedDict):
 
 def embed_documents(config: OllamaEmbedDocumentsConfig) -> List[List[float]]:
     embedder = OllamaEmbeddings(model=config["model"])
-    # embedder = OpenAIEmbeddings()
     embeddings = embedder.embed_documents(config["texts"])
     return embeddings
