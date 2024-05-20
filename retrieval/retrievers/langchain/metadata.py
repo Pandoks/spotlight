9659613e2fb580efd64f6bc0b097e3f876671c28
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-05-20 12:42:05 -0400

    Added metadata retriever

---
+++ retrieval/retrievers/langchain/metadata.py
@@ -0,0 +1,25 @@
+from typing import List, TypedDict
+from langchain.chains.query_constructor.schema import AttributeInfo
+from langchain.retrievers import SelfQueryRetriever
+from langchain_core.documents import Document
+from langchain_core.language_models.llms import LLM
+from langchain_core.vectorstores import VectorStore
+
+
+class MetadataRetrieveConfig(TypedDict):
+    llm: LLM
+    database: VectorStore
+    document_content_description: str
+    metadata_field_info: List[AttributeInfo]
+    prompt: str
+
+
+def retrieve(config: MetadataRetrieveConfig) -> List[Document]:
+    retriever = SelfQueryRetriever.from_llm(
+        config["llm"],
+        config["database"],
+        config["document_content_description"],
+        config["metadata_field_info"],
+    )
+    documents = retriever.invoke(config["prompt"])
+    return documents
