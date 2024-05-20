9a0ac6ec0b8b18f4192ebff914cb99443d1013c5
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-05-20 09:38:20 -0400

    Updated gitignore

--- retrieval/retrievers/langchain/vectorstore.py
+++ retrieval/retrievers/langchain/vectorstore.py
@@ -3,13 +3,13 @@ from langchain_core.documents import Document
 from langchain_core.vectorstores import VectorStore
 
 
-class RetrieveConfig(TypedDict):
+class VectorStoreRetrieveConfig(TypedDict):
     database: VectorStore
     search_type: str
     prompt: str
 
 
-def retrieve(config: RetrieveConfig) -> List[Document]:
+def retrieve(config: VectorStoreRetrieveConfig) -> List[Document]:
     database = config["database"]
     retriever = database.as_retriever(search_type=config["search_type"])
     documents = retriever.invoke(config["prompt"])
